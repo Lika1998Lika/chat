eaf2b00d05615f0c614323120235711cb9c1415f
Anzhelika <anzhelika_vanuyto@mail.ru>
2023-12-19 22:00:36 +0600

    feat: add private route and useAuth hook

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from './pages/SignUpPage';
 import NotFoundPage from "./pages/NotFoundPage";
-
+import AuthContext from '../contexts/AuthContext';
+import PrivateRoute from './PrivateRoute'
+import { useCallback } from 'react';
 const App = () => {
+    const [user, setUser] = useState(null);
+    const logIn = useCallback((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+        setUser(true);
+    }, []);
     return (
         <>
-            <Routes>
-                <Route path='/' element={<LoginPage />} />
-                <Route path='/login' element={<LoginPage />} />
-                <Route path='/sign-up' element={<SignUpPage />} />
-                <Route path='*' element={<NotFoundPage />} />
-            </Routes>
+            <AuthContext.Provider value={{ user, logIn }}>
+                <Routes>
+                    <Route element={<PrivateRoute />}>
+                        <Route path='/' element={<MainPage />} />
+                    </Route>
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/sign-up' element={<SignUpPage />}/>
+                    <Route path='*' element={<NotFoundPage />} />
+                </Routes>
+            </AuthContext.Provider>
         </>
     );
 };
