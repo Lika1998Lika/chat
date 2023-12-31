2a96ff73b69e2007a0a0777cf2ed94c35772f8b1
Anzhelika <anzhelika_vanuyto@mail.ru>
2023-12-31 14:44:24 +0600

    remove useCallback

--- frontend/src/components/AuthProvider.jsx
+++ frontend/src/components/AuthProvider.jsx
@@ -1,26 +1,24 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import {
-  useState, useCallback,
-} from 'react';
+import { useState } from 'react';
 import AuthContext from '../contexts/AuthContext';
 
 const AuthProvider = ({ children }) => {
   const currentUser = JSON.parse(localStorage.getItem('user'));
   const [user, setUser] = useState(currentUser || null);
 
-  const logIn = useCallback((userData) => {
+  const logIn = (userData) => {
     localStorage.setItem('user', JSON.stringify(userData));
     setUser(userData);
-  }, []);
+  };
 
-  const logOut = useCallback(() => {
+  const logOut = () => {
     localStorage.removeItem('user');
     setUser(null);
-  }, []);
+  };
 
-  const getAuthHeaders = useCallback(() => ({
+  const getAuthHeaders = () => ({
     headers: { Authorization: `Bearer ${user.token}` },
-  }), [user]);
+  });
 
   const authValue = {
     user, logIn, logOut, getAuthHeaders,
